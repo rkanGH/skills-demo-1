7ae3eb1c0833fafef527e525ed29e8d3420cdec8
Ryan Kan <kanryan@gmail.com>
2022-02-01 17:57:33 -0800

    demo1

---
+++ demo1.java
@@ -0,0 +1,5 @@
+public class demo1 {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ test1.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class test1 {
+    @Test 
+    public void testMultiply() {
+        assertEquals(5, demo1.multiply(2, 2));
+    }
+}
